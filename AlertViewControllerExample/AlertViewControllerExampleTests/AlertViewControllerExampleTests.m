80b2897fd378c30036b9865477bacc76b6b8630f
c4arl0s <c.santiago.cruz@gmail.com>
2019-04-26 21:20:02 -0600

    Add code

---
+++ AlertViewControllerExample/AlertViewControllerExampleTests/AlertViewControllerExampleTests.m
@@ -0,0 +1,37 @@
+//
+//  AlertViewControllerExampleTests.m
+//  AlertViewControllerExampleTests
+//
+//  Created by Carlos Santiago Cruz on 4/17/19.
+//  Copyright © 2019 Carlos Santiago Cruz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AlertViewControllerExampleTests : XCTestCase
+
+@end
+
+@implementation AlertViewControllerExampleTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
