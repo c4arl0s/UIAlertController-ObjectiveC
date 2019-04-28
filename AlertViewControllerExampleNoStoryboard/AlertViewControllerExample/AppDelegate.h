74307b5d413003b13f9e10d7fbb0db1a13b45d38
c4arl0s <c.santiago.cruz@gmail.com>
2019-04-27 18:32:04 -0600

    Update code

---
+++ AlertViewControllerExampleNoStoryboard/AlertViewControllerExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlertViewControllerExample
+//
+//  Created by Carlos Santiago Cruz on 4/17/19.
+//  Copyright © 2019 Carlos Santiago Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AlertViewControllerExampleNoStoryboard/AlertViewControllerExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlertViewControllerExample
+//
+//  Created by Carlos Santiago Cruz on 4/17/19.
+//  Copyright © 2019 Carlos Santiago Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
